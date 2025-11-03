3f31e6417a7b6cba0d5751031bc3731cc034f117
Samoshechkin <samoshechkin@gmail.com>
2025-11-03 11:54:15 +0300

    fix: удалить дублирование ограничения размера чанка в конфигурации сборки

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
 
   // ⚙️ Игнорируем ворнинги вида "use client" из framer-motion
   build: {
+    chunkSizeWarningLimit: 1200,
     rollupOptions: {
       onwarn(warning, warn) {
         if (
